5183ab2758629c5d460aff520bd55b1203b113cc
Santosh Purbey <purbey.santosh@hotmail.com>
2023-08-25 19:22:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-recipe-32",
+  url: "https://snowy-recipe-32.botics.co",
+  api: "https://snowy-recipe-32.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
